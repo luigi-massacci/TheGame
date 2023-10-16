730bb593c2cf2125e155cac6e665d6f3e5568eb9
Natali124 <nataligogishvili@gmail.com>
2023-10-16 17:25:51 +0200

    game

--- GameTree.hs
+++ GameTree.hs
@@ -13,4 +13,11 @@ import Types
 import Nodes
 
 -- pre-generated game tree structure
-gameTree :: Tree TreeNode = Node (root) []
+gameTree :: Tree TreeNode = Node (root) [Node root [], Node midgard []]
+
+-- we need smth like 'entry' which runs when we enter the node
+
+runGame :: IO()
+runGame = do
+    putStrLn "Welcome to Binary Tree World.\n"
+    putStrLn "You are at the root of an ancient binary tree."
