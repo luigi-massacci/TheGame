2de8dd68bd842a7299f6e4f5784ed90988ecb472
Luigi Massacci <luigi.massacci.it@gmail.com>
2023-10-16 17:14:32 +0200

    Pregenerated nodes

--- Nodes.hs
+++ Nodes.hs
@@ -14,5 +14,15 @@ import Types
 -- Initial nodes
 
 root :: TreeNode = TreeNode "Root" PlatformNode "You are in the root, you can go somewhere!"
-midgard :: TreeNode = TreeNode "Midgard" PlatformNode "You've reached Asgard, home of the gods!"
--- add mode pre-generated nodes here
+
+midgard :: TreeNode = TreeNode "Midgard" PlatformNode "You've reached Midgard, the mortal plane!"
+
+asgard :: TreeNode = TreeNode "Asgard" PlatformNode "You've reached Asgard, home of the Gods!"
+
+swartelheim :: TreeNode = TreeNode "Swartelheim" PlatformNode "You've reached Swartelheim, land of the dwarves!"
+
+musplheim :: TreeNode = TreeNode "Musplheim" PlatformNode "You've reached Musplheim, land of the fire giants!"
+
+helheim :: TreeNode = TreeNode "Helheim" PlatformNode "You've reached Helheim, land of the dead!"
+
+alvheim :: TreeNode = TreeNode "Alvheim" PlatformNode "You've reached Alvehim, land of the elves"
