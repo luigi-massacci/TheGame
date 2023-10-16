0101eebd236a3e9d4b1ad1645600b8e42475c87b
Natali124 <nataligogishvili@gmail.com>
2023-10-16 17:04:59 +0200

    nodes

--- Nodes.hs
+++ Nodes.hs
@@ -2,6 +2,8 @@
 
 {-# HLINT ignore "Use newtype instead of data" #-}
 
+module Nodes where
+
 import Data.List
 import Data.Set (fromList)
 import Data.Tree
@@ -9,4 +11,8 @@ import GHC.Core.TyCon (newTyConEtadArity)
 import System.Random
 import Types
 
+-- Initial nodes
+
+root :: TreeNode = TreeNode "Root" PlatformNode "You are in the root, you can go somewhere!"
 midgard :: TreeNode = TreeNode "Midgard" PlatformNode "You've reached Asgard, home of the gods!"
+-- add mode pre-generated nodes here
